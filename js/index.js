5b8fcbad00c9e82c4c2b9ca2e79480f7dbc056d5
dsy4567 <dsy4567@outlook.com>
2022-12-30 10:39:04 +0000

    .

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
+// @ts-check
 async function 获取名言() {
+    // @ts-ignore
     return new Promise((resolve, reject) => {
         fetch("/json/saying.json")
             .then((res) => res.json())
@@ -8,18 +10,21 @@ async function 获取名言() {
                         "获取失败"
                 );
             })
+            // @ts-ignore
             .catch((e) => {
                 resolve("获取失败");
             });
     });
 }
 async function 获取个人信息() {
+    // @ts-ignore
     return new Promise((resolve, reject) => {
         fetch("https://api.github.com/users/dsy4567")
             .then((res) => res.json())
             .then((j) => {
                 resolve(j);
             })
+            // @ts-ignore
             .catch((e) => {
                 resolve({ bio: "", followers: "未知", following: "未知" });
             });
@@ -27,15 +32,27 @@ async function 获取个人信息() {
 }
 完成加载.push(async () => {
     if (路径 == "/") {
+        // @ts-ignore
         document.querySelector("#随机金句").innerHTML =
             "&emsp;&emsp;" + (await 获取名言());
+
         let 个人信息 = await 获取个人信息();
+        // @ts-ignore
         document.querySelector("#关注粉丝码龄").innerHTML = ` 关注: ${
             个人信息.following
         } | 粉丝: ${个人信息.followers} | 码龄: ${
             new Date().getFullYear() -
             new Date(个人信息.created_at).getFullYear()
         }年 `;
-        document.querySelector("#个性签名").innerText = 个人信息.bio;
+
+        // @ts-ignore
+        document.querySelector("#个性签名").innerText = "";
+        let arr = [...String(个人信息.bio)],
+            interval = setInterval(() => {
+                let t = arr.shift();
+                t || clearInterval(interval);
+                // @ts-ignore
+                document.querySelector("#个性签名").innerText += t || "";
+            }, 3000 / arr.length);
     }
 });
