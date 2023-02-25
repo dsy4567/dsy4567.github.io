bbf938771165d86538f4f551e32c71bb7502aef3
dsy4567 <dsy4567@outlook.com>
2023-02-25 15:32:45 +0800

    .

--- api/ncm-cover.ts
+++ api/ncm-cover.ts
@@ -3,15 +3,19 @@ export const config = {
 };
 
 export default async (request: Request) => {
-    let u = new URL("" + request.url);
-    let id = u.pathname.replace("/", "");
-    let s = await (await fetch("https://music.163.com/song?id=" + id)).text();
+    let u = new URL(request.url);
+    let s = await (
+        await fetch("https://music.163.com/song?id=" + u.searchParams.get("id"))
+    ).text();
     let m = s.match(
         /<meta property="og:image" content="http:\/\/.+\.jpg" \/>/s
     );
     return fetch(
         m?.[0]
             .replace('<meta property="og:image" content="', "")
-            .replace('" />', "") + u.search
+            .replace('" />', "") +
+            (u.searchParams.get("size") || "64") +
+            "y" +
+            (u.searchParams.get("size") || "64")
     );
 };
