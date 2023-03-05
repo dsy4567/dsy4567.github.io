e01ab20f99e4b330fe74e700f8d02de82127eec6
dsy4567 <dsy4567@outlook.com>
2023-03-05 16:21:02 +0800

    Update gravatar.ts

--- api/gravatar.ts
+++ api/gravatar.ts
@@ -1,7 +1,7 @@
 export const config = {
     runtime: "edge",
 };
-export default async (request: Request) => {
+export default async (request) => {
     return fetch(
         "https://s.gravatar.com/avatar/9b52496d475f92cafb75ea1f822c0b74" +
             new URL(request.url).search
