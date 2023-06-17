154996c82e32284695e8d583a58445d1f70197f9
dsy4567 <dsy4567@outlook.com>
2023-06-17 18:39:03 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -209,7 +209,7 @@ fetch("/json/theme.json")
     .then(res => res.json())
     .then(主题 => {
         Object.keys(主题).forEach(t => {
-            let btn = ce("button");
+            let btn = ce("span");
             btn.style.backgroundColor = 主题[t]["--theme-color"];
             btn.title = t;
             btn.role = "radio";
