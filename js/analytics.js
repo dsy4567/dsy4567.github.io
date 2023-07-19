005b2b65788dc9ab55c35884bb27a9140eb1731f
dsy4567 <dsy4567@outlook.com>
2023-07-19 18:01:05 +0800

    更新统计代码

--- js/analytics.js
+++ js/analytics.js
@@ -1,7 +1,7 @@
 export default (() => {
     if (location.hostname === "dev.dsy4567.cf") return;
-    let /** @type {HTMLScriptElement} */ s1 = ce("script"),
-        /** @type {HTMLScriptElement} */ s2 = ce("script");
+    let /** @type {HTMLScriptElement} */ s1 = document.createElement("script"),
+        /** @type {HTMLScriptElement} */ s2 = document.createElement("script");
     s1.async = s1.defer = s2.async = s2.defer = true;
     s1.src = "https://www.googletagmanager.com/gtag/js?id=G-060YCRMSSH";
     s2.src = "https://dsy4567.cf/_vercel/insights/script.js";
