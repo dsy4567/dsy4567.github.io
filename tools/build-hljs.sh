7650a11f59468197f1463fde57a63fcf27f4c191
dsy4567 <dsy4567@outloo.com>
2023-08-01 16:39:35 +0800

    更新highlight.js及主题

---
+++ tools/build-hljs.sh
@@ -0,0 +1,4 @@
+#!/data/data/com.termux/files/usr/bin/bash
+cd ~/highlight.js/
+node ~/highlight.js/tools/build.js bash c csharp cpp css diff go graphql xml json java javascript kotlin less lua makefile markdown objectivec php perl plaintext python r ruby rust scss sql shell swift ini typescript yaml properties nginx
+cp ~/highlight.js/build/highlight.min.js ~/dsy4567.github.io/js/
