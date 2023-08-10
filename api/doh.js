1aae14cc9a26ea46f2d744e69d0c9aafe509a00a
dsy4567 <dsy4567@outlook.com>
2023-08-10 20:45:42 +0800

    Update doh.js

--- api/doh.js
+++ api/doh.js
@@ -1,6 +1,12 @@
 async function f(req) {
-    return fetch('https://1.1.1.1/dns-query',
-                 {method:req.method,body:req.body})
+    let u = new URL(req.url, 'https://dsy4567.cf/api/doh')
+    return fetch(
+        'https://1.1.1.1/dns-query' + u.search,
+        {
+            method: req.method,
+            body: req.body
+        }
+    )
 }
 f.fetch = f;
 export const config = {
