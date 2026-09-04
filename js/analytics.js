77e109e446f6079bcfea4233d311be5f834a8885
dsy4567 <dsy4567@outlook.com>
2026-09-04 14:14:48 +0800

    ⚡️ 调整谷歌分析和cf脚本的加载属性
    - 移除两个脚本的defer属性，仅保留async属性
    - 优化脚本加载的并行执行效率

--- js/analytics.js
+++ js/analytics.js
@@ -13,7 +13,7 @@ export default (() => {
 	if (location.hostname !== "dsy4567.icu" && location.hostname !== "dsy4567.github.io") return;
 	const /** @type {HTMLScriptElement} */ s1 = document.createElement("script"),
 		ga_id = location.hostname === "dsy4567.icu" ? "G-9E9HPL4Y86" : "G-N7WR0PVV4J";
-	s1.async = s1.defer = true;
+	s1.async = true;
 	s1.src = "https://www.googletagmanager.com/gtag/js?id=" + ga_id;
 	document.body.append(s1);
 	window.dataLayer = window.dataLayer || [];
@@ -24,7 +24,7 @@ export default (() => {
 	gtag("config", ga_id);
 
 	const /** @type {HTMLScriptElement} */ s2 = document.createElement("script");
-	s2.defer = true;
+	s2.async = true;
 	s2.setAttribute("data-cf-beacon", '{"token": "9ef701d58e594dcc960850227e0f2436"}');
 	s2.src = "https://static.cloudflareinsights.com/beacon.min.js";
 
