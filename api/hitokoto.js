99f7d291d67d52442bfbfd23bd39c279bbdbfa9c
dsy4567 <dsy4567@outlook.com>
2026-09-08 00:53:36 +0800

    🐛 修复一言接口响应缺少 Content-Type 的问题
    - 为 hitokoto API 的 Response 添加 Content-Type: application/json; charset=UTF-8 响应头
    - 调整代码缩进为 tab

--- api/hitokoto.js
+++ api/hitokoto.js
@@ -1,28 +1,30 @@
 function r(max) {
-    return Math.floor(Math.random() * max + 1);
+	return Math.floor(Math.random() * max + 1);
 }
 async function f(req) {
-    return new Promise((res, rej) => {
-        fetch(
-            "https://cdn.jsdelivr.net/gh/hitokoto-osc/sentences-bundle@1.0.306/categories.json"
-        )
-            .then(resp => resp.json())
-            .then(j => {
-                fetch(
-                    "https://cdn.jsdelivr.net/gh/hitokoto-osc/sentences-bundle@1.0.306/" +
-                        (j[r(j.length)] || j[0]).path
-                )
-                    .then(resp => resp.json())
-                    .then(j => {
-                        res(
-                            new Response(JSON.stringify(j[r(j.length)] || j[0]))
-                        );
-                    });
-            });
-    });
+	return new Promise((res, rej) => {
+		fetch("https://cdn.jsdelivr.net/gh/hitokoto-osc/sentences-bundle@1.0.306/categories.json")
+			.then(resp => resp.json())
+			.then(j => {
+				fetch(
+					"https://cdn.jsdelivr.net/gh/hitokoto-osc/sentences-bundle@1.0.306/" +
+						(j[r(j.length)] || j[0]).path
+				)
+					.then(resp => resp.json())
+					.then(j => {
+						res(
+							new Response(JSON.stringify(j[r(j.length)] || j[0]), {
+								headers: {
+									"Content-Type": "application/json; charset=UTF-8",
+								},
+							})
+						);
+					});
+			});
+	});
 }
 f.fetch = f;
 export const config = {
-    runtime: "edge",
+	runtime: "edge",
 };
 export default f;
